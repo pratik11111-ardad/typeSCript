31f0c8065316b1e7b84b5cc52fabbc7c6e4125dc
Pratik Ardad <Pratikardad1104@gmail.com>
2026-06-08 21:24:19 +0530

    remaining file

---
+++ Day4/incrementDcreament.ts
@@ -0,0 +1,24 @@
+let num =10;
+console.log(num++); //post increment operator returns the value of num and then increments it by 1
+
+let num1 =10;
+    console.log(++num1); //pre increment operator increments the value of num1 by 1 and then returns it
+
+    let num2 =40;
+    console.log(num2++ + ++num2);
+
+    
+
+    let num3=50;
+    --num3;
+    console.log(num3);
+
+    console.log(num3-- +--num3); //49+47 post decrement operator returns the value of num3 and then decrements it by 1, pre decrement operator decrements the value of num3 by 1 and then returns it 
+    
+    let num4=50;
+    let y=num4--;
+    let z=--num4;
+    console.log(y+z);
+
+
+    
---
+++ Day4/logicalOperator.ts
@@ -0,0 +1,10 @@
+let aq=10, ar=20, as=30;
+if(aq>ar && aq>as){
+    console.log("aq is the greatest number");
+}
+else if(ar>aq && ar>as){
+    console.log("ar is the greatest number");
+}
+else {
+    console.log("as is the greatest number");
+}
