44837b98e9722afbe8da3c7a29032a23c7209737
Pratik Ardad <Pratikardad1104@gmail.com>
2026-06-09 08:46:16 +0530

    adding file

---
+++ Day4/ifElseAndNestedIfElse.ts
@@ -0,0 +1,38 @@
+// let Age=18;
+// if(Age>17){
+//     console.log("adult");
+
+// }
+// else{
+//     console.log("minor");
+// }
+
+// let age=20;
+// if(age>18){
+//     console.log("you can date");
+// }
+// else if(age>18 && age<20){
+//     console.log("you can date");
+// }
+// else{
+//     console.log("you can married");
+// }
+
+// let browser="edge";
+// if(browser==="firefor"){
+//     console.log("open firefox");
+// }
+// else if(browser==="chrome"){
+//     console.log("open chrome");
+
+// }
+// else{
+//     console.log("open edge");
+// }
+ 
+if(false || true && false){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
